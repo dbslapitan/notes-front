048d7285263d3961f20790b370b4c96c8b3c7886
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-10 01:50:25 -0400

    change tab triggers to link

--- src/ui/mobile-layout.tsx
+++ src/ui/mobile-layout.tsx
@@ -10,41 +10,43 @@ import { MouseEvent, useRef, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import Tags from "./tags";
 import MobileSettings from "./mobile-settings";
+import Link from "next/link";
 
 export default function MobileLayout({notes, tags}: {notes: INote[], tags: string[]}){
 
   const tabRef = useRef<null | HTMLDivElement>(null);
-  const router = useRouter();
-  const path = usePathname();
   const searchParams = useSearchParams();
 
   const currentParam = searchParams.has("search") && "search" || searchParams.has("tag") && "tag" || searchParams.has("archived") && "archived" || "home";
 
-  const [value, setValue] = useState(currentParam === "tag" ? "tags" : currentParam);
+  const value = currentParam === "tag" ? "tags" : currentParam;
 
-  const tabs = [{ value: "home", class: `before:[mask-image:url("/icons/icon-home.svg")]` }, { value: "search", class: `before:[mask-image:url("/icons/icon-search.svg")]` }, { value: "archived", class: `before:[mask-image:url("/icons/icon-archive.svg")]` }, { value: "tags", class: `before:[mask-image:url("/icons/icon-tag.svg")]` }, { value: "settings", class: `before:[mask-image:url("/icons/icon-settings.svg")]` }];
-
-  const onTabClickHander = (event: MouseEvent, tab: string) => {
-    if(tab === "home"){
-      setValue("home")
-      router.push(`${path}`);
-    }
-    else if(tab === "search"){
-      setValue("search")
-      router.push(`${path}?search=`);
-    }
-    else if(tab === "archived"){
-      setValue("archived")
-      router.push(`${path}?archived`);
-    }
-    else if(tab === "tags"){
-      setValue("tags")
-      router.push(`${path}?tag=`);
-    }
-    else if(tab == "settings"){
-      setValue("settings")
-    }
-  }
+  const tabs = [
+    { 
+      value: "home", 
+      class: `before:[mask-image:url("/icons/icon-home.svg")]`,
+      href: "/preview"
+    }, 
+    { 
+      value: "search", 
+      class: `before:[mask-image:url("/icons/icon-search.svg")]`, 
+      href: "/preview?search="
+    }, 
+    { 
+      value: "archived", 
+      class: `before:[mask-image:url("/icons/icon-archive.svg")]`, 
+      href: "/preview?archived"
+    }, 
+    { 
+      value: "tags", 
+      class: `before:[mask-image:url("/icons/icon-tag.svg")]`, 
+      href: "/preview?tag="
+    }, 
+    { 
+      value: "settings", 
+      class: `before:[mask-image:url("/icons/icon-settings.svg")]`, 
+      href: "/preview/settings"
+    }];
 
   return(
     <>
@@ -56,8 +58,10 @@ export default function MobileLayout({notes, tags}: {notes: INote[], tags: strin
               tabs.map(tab => {
                 return(
                   <div key={tab.value} className="w-full h-fit text-center border-r border-r-transparent last:border-0 md:border-r-neutral-200 md:dark:border-r-neutral-800">
-                    <TabsTrigger onClick={(e) => onTabClickHander(e, tab.value)} value={tab.value} className={`relative w-full h-clamp1 p-0 rounded-1 max-w-20 m-auto ${tab.class} data-[state=active]:bg-blue-50 data-[state=active]:text-blue-700 before:content-[" "] before:block before:w-full before:h-full before:bg-neutral-950 before:[mask-position:center_0.25rem] before:[mask-repeat:no-repeat] data-[state=active]:before:bg-blue-700 data-[state=active]:dark:shadow-none dark:data-[state=active]:bg-neutral-700 dark:text-[#99A0AE] dark:before:bg-[#99A0AE] dark:data-[state=active]:text-blue-500`}>
-                      <span className={`absolute bottom-1 capitalize invisible md:visible ${text["preset-6"]} md:`}>{tab.value}</span>
+                    <TabsTrigger value={tab.value} asChild className={`relative w-full h-clamp1 p-0 rounded-1 max-w-20 m-auto ${tab.class} data-[state=active]:bg-blue-50 data-[state=active]:text-blue-700 before:content-[" "] before:block before:w-full before:h-full before:bg-neutral-950 before:[mask-position:center_0.25rem] before:[mask-repeat:no-repeat] data-[state=active]:before:bg-blue-700 data-[state=active]:dark:shadow-none dark:data-[state=active]:bg-neutral-700 dark:text-[#99A0AE] dark:before:bg-[#99A0AE] dark:data-[state=active]:text-blue-500`}>
+                      <Link href={tab.href}>
+                        <span className={`absolute bottom-1 capitalize invisible md:visible ${text["preset-6"]} md:`}>{tab.value}</span>
+                      </Link>
                     </TabsTrigger>
                   </div>
                 )
