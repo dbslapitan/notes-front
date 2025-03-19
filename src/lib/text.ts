8f9817bb212004bf7ac5954b17ce90768d51f25e
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-19 12:13:17 -0400

    add text preset

---
+++ src/lib/text.ts
@@ -0,0 +1,8 @@
+export const text = {
+  "preset-1": "text-2xl/[1.2] -tracking-[0.5px]",
+  "preset-2": "text-xl/[1.2] -tracking-[0.5px]",
+  "preset-3": "text-base/[1.2] -tracking-[0.3px]",
+  "preset-4": "text-sm/[1.2] -tracking-[0.2px]",
+  "preset-5": "text-sm/[1.2] -tracking-[0.2px]",
+  "preset-6": "text-xs/[1.2] -tracking-[0.2px]",
+};
