243ee57bee4f9e55fdffe462b79050e7e433ca4b
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-19 15:57:06 -0400

    add design of nav on tab

--- src/ui/navigation.tsx
+++ src/ui/navigation.tsx
@@ -4,39 +4,40 @@ import SearchSVG from "./svgs/search";
 import ArchiveSVG from "./svgs/archive";
 import TagsSVG from "./svgs/tag";
 import SettingsSVG from "./svgs/settings";
+import { text } from "@/lib/text";
 
 export default function Navigation({selected = null}: {selected: string | null}){
   return(
-    <nav className="shadow-sm py-3 px-4 border-t border-t-neutral-200 bg-neutral-0">
+    <nav className="shadow-sm py-3 px-4 md:px-0 border-t border-t-neutral-200 bg-neutral-0">
       <ul className="flex">
-        <li className={`grow rounded-sm ${selected === "home" ? "bg-blue-50" : ""}`}>
-          <Link className={`flex flex-col justify-center items-center py-1 ${selected === "home" ? "text-blue-500" : "text-neutral-950"}`} href={`/preview`}>
+        <li className={`grow border-r border-neutral-200`}>
+          <Link className={`flex flex-col gap-1 justify-center items-center py-1 rounded-sm max-w-[53.33%] m-auto ${selected === "home" ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/preview`}>
             <HomeSVG />
-            <span className="hidden md:block">Home</span>
+            <span className={`hidden md:block ${text["preset-6"]}`}>Home</span>
           </Link>
         </li>
-        <li className={`grow rounded-sm ${selected === "search" ? "bg-blue-50" : ""}`}>
-          <Link className={`flex flex-col justify-center items-center py-1 ${selected === "search" ? "text-blue-500" : "text-neutral-950"}`} href={`/preview?search=`}>
+        <li className={`grow border-r border-neutral-200`}>
+          <Link className={`flex flex-col gap-1 justify-center items-center py-1 rounded-sm max-w-[53.33%] m-auto ${selected === "search" ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/preview?search=`}>
             <SearchSVG />
-            <span className="hidden md:block">Search</span>
+            <span className={`hidden md:block ${text["preset-6"]}`}>Search</span>
           </Link>
         </li>
-        <li className={`grow rounded-sm ${selected === "archived" ? "bg-blue-50" : ""}`}>
-          <Link className={`flex flex-col justify-center items-center py-1 ${selected === "archived" ? "text-blue-500" : "text-neutral-950"}`} href={`/preview?archived`}>
+        <li className={`grow border-r border-neutral-200`}>
+          <Link className={`flex flex-col gap-1 justify-center items-center py-1 rounded-sm max-w-[53.33%] m-auto ${selected === "archived" ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/preview?archived`}>
             <ArchiveSVG />
-            <span className="hidden md:block">Archived</span>
+            <span className={`hidden md:block ${text["preset-6"]}`}>Archived</span>
           </Link>
         </li>
-        <li className={`grow rounded-sm ${selected === "tags" ? "bg-blue-50" : ""}`}>
-          <Link className={`flex flex-col justify-center items-center py-1 ${selected === "tags" ? "text-blue-500" : "text-neutral-950"}`} href={`/preview?tag=`}>
+        <li className={`grow border-r border-neutral-200`}>
+          <Link className={`flex flex-col gap-1 justify-center items-center py-1 rounded-sm max-w-[53.33%] m-auto ${selected === "tags" ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/preview?tag=`}>
             <TagsSVG />
-            <span className="hidden md:block">Tags</span>
+            <span className={`hidden md:block ${text["preset-6"]}`}>Tags</span>
           </Link>
         </li>
-        <li className={`grow rounded-sm ${selected === "settings" ? "bg-blue-50" : ""}`}>
-          <Link className={`flex flex-col justify-center items-center py-1 ${selected === "settings" ? "text-blue-500" : "text-neutral-950"}`} href={`/settings`}>
+        <li className={`grow`}>
+          <Link className={`flex flex-col gap-1 justify-center items-center py-1 rounded-sm max-w-[53.33%] m-auto ${selected === "settings" ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/settings`}>
             <SettingsSVG />
-            <span className="hidden md:block">Settings</span>
+            <span className={`hidden md:block ${text["preset-6"]}`}>Settings</span>
           </Link>
         </li>
       </ul>
