a868f74386dc5a695aa5b6de77019e4aa80a95e3
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-22 06:31:08 -0400

    move notes list diplay to a component

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,23 +1,18 @@
 import { URI } from "@/lib/constants";
 import { text } from "@/lib/text";
 import { INote } from "@/models/note";
-import { Badge } from "@/ui/badge";
-import { ScrollArea } from "@/ui/scroll-area";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import Notes from "@/ui/notes";
 
-export default async function Page({params}: {params: Promise<{username: string}>}) {
+export default async function Page({ params }: { params: Promise<{ username: string }> }) {
 
-  const {username} = await params;
+  const { username } = await params;
 
-  const { notes }: { notes: INote[] } = await fetch(`${URI}/api/v1/${username}`, { method: "GET", cache: "force-cache"}).then(res => res.json());
-
-  if(notes.length){
-    redirect(`/${username}/${notes[0]._id}`);
-  }
+  const { notes }: { notes: INote[] } = await fetch(`${URI}/api/v1/${username}`, { method: "GET", cache: "force-cache" }).then(res => res.json());
 
   return (
-    <>
-    </>
+    <main className="flex flex-col grow px-4 pt-5 bg-neutral-0 rounded-2xl overflow-y-hidden">
+      <h1 className={`${text["preset-1"]}`}>All Notes</h1>
+      <Notes notes={notes} username={username} />
+    </main>
   );
 }
---
+++ src/ui/notes.tsx
@@ -0,0 +1,39 @@
+import { INote } from "@/models/note";
+import { ScrollArea } from "./scroll-area";
+import Link from "next/link";
+import { text } from "@/lib/text";
+import { Badge } from "./badge";
+
+export default function ({notes, username}: {notes: INote[], username: string}){
+  return(
+    <ScrollArea className="grow mt-4 pb-5 h-[535px]">
+    <ul>
+      {
+        notes.map(note => {
+          return (
+            <li key={note._id} className={`note border-t border-t-neutral-200 first-of-type:border-none [.selected+li]:border-none`}>
+              <Link href={`/${username}?selected=${note._id}`} className={`block pt-2 px-2 pb-3`}>
+                <h2 className={`${text["preset-3"]}`}>{note.title}</h2>
+                <ul className="mt-3 flex flex-wrap gap-1">
+                  {
+                    note.tags.map(tag => {
+                      return (
+                        <Badge key={tag} asChild className={`bg-neutral-200 text-neutral-950 ${text["preset-6"]}`}>
+                          <li>
+                            {tag}
+                          </li>
+                        </Badge>
+                      )
+                    })
+                  }
+                </ul>
+                <p className={`mt-3 ${text["preset-6"]}`}>{new Date(note.lastEdited).toLocaleDateString("en-GB", { day: "numeric", month: "short", year: "numeric" })}</p>
+              </Link>
+            </li>
+          );
+        })
+      }
+    </ul>
+  </ScrollArea>
+  );
+}
