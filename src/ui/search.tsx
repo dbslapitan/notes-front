792ad6185730d36beb653a807613e5453d4d25ea
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-23 03:45:58 -0400

    add display when no notes found

--- src/ui/search.tsx
+++ src/ui/search.tsx
@@ -4,6 +4,7 @@ import { Input } from "./input";
 import SearchInput from "./search-input";
 import { INote } from "@/models/note";
 import Notes from "./notes";
+import Link from "next/link";
 
 export default function Search({notes, username, value}: {notes: INote[], username: string, value: string}){
   
@@ -14,7 +15,9 @@ export default function Search({notes, username, value}: {notes: INote[], userna
       {
         value.trim() && <p className={`mt-4 ${text["preset-5"]} text-neutral-700`}>All notes matching "{value}" are displayed below.</p>
       }
-      <Notes notes={notes} username={username} />
+      {
+        notes.length ? <Notes notes={notes} username={username} /> : <p className={`mt-4 p-2 rounded-[0.5rem] bg-neutral-100 ${text["preset-5"]}`}>No notes match your search. Try a different keyword or <Link className="underline" href={`${username}/create`}>create a new note.</Link></p>
+      }
     </Main>
   );
 }
