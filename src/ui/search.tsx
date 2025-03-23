c43c35d7bd42ebb8241ece19f47667e06dc067d8
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-23 05:43:35 -0400

    add scroll wrapper on arhived and search

--- src/ui/search.tsx
+++ src/ui/search.tsx
@@ -4,6 +4,7 @@ import SearchInput from "./search-input";
 import { INote } from "@/models/note";
 import Notes from "./notes";
 import Link from "next/link";
+import ScrollWrapper from "./scroll-wrapper";
 
 export default function Search({notes, username, value}: {notes: INote[], username: string, value: string}){
   
@@ -15,7 +16,7 @@ export default function Search({notes, username, value}: {notes: INote[], userna
         value.trim() && <p className={`mt-4 ${text["preset-5"]} text-neutral-700`}>All notes matching {`"${value}"`} are displayed below.</p>
       }
       {
-        notes.length ? <Notes notes={notes} username={username} /> : <p className={`mt-4 p-2 rounded-[0.5rem] bg-neutral-100 ${text["preset-5"]}`}>No notes match your search. Try a different keyword or <Link className="underline" href={`${username}/create`}>create a new note.</Link></p>
+        notes.length ? <ScrollWrapper bottomId={"#mobile-nav"}><Notes notes={notes} username={username} /></ScrollWrapper> : <p className={`mt-4 p-2 rounded-[0.5rem] bg-neutral-100 ${text["preset-5"]}`}>No notes match your search. Try a different keyword or <Link className="underline" href={`${username}/create`}>create a new note.</Link></p>
       }
     </Main>
   );
