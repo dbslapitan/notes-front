8bf58df7a1871085090e4b2ec8e25109c89b47e2
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-31 03:57:59 -0400

    add dark mode on nav

--- src/ui/nav-links.tsx
+++ src/ui/nav-links.tsx
@@ -16,7 +16,7 @@ export default function NavLink({children, link, href}: {children: ReactNode, li
   const resolvedParam = path.includes(`/${username}/settings`) && "settings" || params.has("search") && "search" || params.has("tag") && "tags" || params.has("archived") && "archived" || "home";
 
   return(
-    <Link className={`${fonts[font]} flex flex-col gap-1 justify-center items-center py-1 rounded-sm md:max-w-[53.33%] m-auto ${resolvedParam === link ? "bg-blue-50 text-blue-500" : "text-neutral-950"}`} href={`/${username}${href}`}>
+    <Link className={`${fonts[font]} flex flex-col gap-1 justify-center items-center py-1 rounded-sm md:max-w-[53.33%] m-auto ${resolvedParam === link ? "bg-blue-50 text-blue-500 dark:bg-neutral-700" : "text-neutral-950 dark:text-[#99A0AE]"}`} href={`/${username}${href}`}>
       {children}
     </Link>
   );
